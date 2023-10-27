fc92970dba5c15d51049da2dd2b7d4f90ffe02b7
Kawexds <kawecarvalho68@gmail.com>
2023-10-27 00:50:09 -0300

    mudança de nome

--- SourceCodeInspection/src/br/calebe/teste3/TicketMachineTest.java
+++ SourceCodeInspection/src/br/calebe/teste3/TicketMachineTest.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
-public class TicketMachineTeste {
+public class TicketMachineTest {
 
     private TicketMachine ticketMachine;
 
