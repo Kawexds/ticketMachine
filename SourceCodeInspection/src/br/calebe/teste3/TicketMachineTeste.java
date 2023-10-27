772943b3c45688323fd33027510c3f2f908767f9
Kawe Carvalho <73030689+Kawexds@users.noreply.github.com>
2023-10-27 00:28:48 -0300

    Update TicketMachineTeste.java

--- SourceCodeInspection/src/br/calebe/teste3/TicketMachineTeste.java
+++ SourceCodeInspection/src/br/calebe/teste3/TicketMachineTeste.java
@@ -1,4 +1,4 @@
-package br.calebe.ticketmachine.core;
+package br.calebe.teste3;
 
 import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
 import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
