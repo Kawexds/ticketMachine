5624f166f90eafb563044bfd327cfb5d7e000bba
Kawe Carvalho <73030689+Kawexds@users.noreply.github.com>
2023-10-27 21:28:52 -0300

    Add files via upload

---
+++ ticketMachine-main/ticketMachine-main/SourceCodeInspection/src/br/calebe/teste2/core/PapelMoeda.java
@@ -0,0 +1,24 @@
+package br.calebe.teste2.core;
+
+/**
+ *
+ * @author Calebe de Paula Bianchini
+ */
+public class PapelMoeda {
+
+    protected int valor;
+    protected int quantidade;
+
+    public PapelMoeda(int valor, int quantidade) {
+        this.valor = valor;
+        this.quantidade = quantidade;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+}
